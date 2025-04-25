3f73a255601fb939817fe15b1dc4ce95a2404de5
Your Name <you@example.com>
2025-04-25 12:31:30 -0400

    Basic prototype

--- app/app.tsx
+++ app/app.tsx
@@ -1,11 +1,12 @@
-import { Stack } from 'expo-router';
+// app/_layout.tsx
+import { Slot } from 'expo-router';
 import { PaperProvider } from 'react-native-paper';
 import { theme } from './theme';
 
-export default function Layout() {
+export default function RootLayout() {
   return (
     <PaperProvider theme={theme}>
-      <Stack screenOptions={{ headerShown: false }} />
+      <Slot />
     </PaperProvider>
   );
 }
